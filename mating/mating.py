152f82605e5943fa46de3c1e4ec9054ff5850921
Snowball <kai890409@gmail.com>
2021-05-29 20:50:47 +0800

    Done mating feature - Tournament Selection

--- mating/mating.py
+++ mating/mating.py
@@ -1,4 +1,17 @@
 import numpy as np
 
+from random import sample
+
 def mating(data, score):
-    return
+    #tournament selection
+    pairSets = []
+    n = len(score)
+
+    for i in range(n):
+        pair = []
+        while len(pair) < 2:
+            randIndex = sample(range(len(score)), 2)
+            if score[randIndex[0]] >= score[randIndex[1]]: pair.append(data[randIndex[0]])
+            else: pair.append(data[randIndex[1]])
+        pairSets.append(pair)
+    return pairSets
