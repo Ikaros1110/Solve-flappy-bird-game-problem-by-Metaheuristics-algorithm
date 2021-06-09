b94765a22937a0ea1c8b17476089456479a62a69
Ikaros1110 <40647020s+Ikaros1110@users.noreply.github.com>
2021-06-10 04:33:15 +0800

    Update evalution.py

--- evalution.py
+++ evalution.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pygame
 
 class evaluation:
     def __init__(self):
@@ -14,7 +15,7 @@ class evaluation:
         self.game.play()
         
         #calculate answer
-        return self.target_func(self.game.get_die_state())
+        #return self.target_func(self.game.get_die_state())
     
     @staticmethod
     def target_func(state):
@@ -33,10 +34,6 @@ class miniGame:
         self.__state = []# State need to train
         self.__dieState = []# Die State, key-value list
 
-        #instant
-        self.SCREENWIDTH = 288
-        self.PLAYERX = self.SCREENWIDTH * 0.2
-
     def set_state(self, data):
         #set game state
         self.__state = data
@@ -47,5 +44,61 @@ class miniGame:
 
     def play(self):
         #simulate game playing
-        return
-    
+
+        ### set game parameter ###
+        # __state.data    [bird Y , bird speed, pipe X, pipe Y,  isFlap ]
+        # @@ Upper left corner is (0,0) @@
+        # pipe  width is 52(form spirit/pipe.png), Two pipe gap is 100 (from origin source code).
+        birdX = 0
+        birdY = self.__state[0]
+        birdVelX = 14 #Use pipe shirt in origin game
+        birdVelY = self.__state[1]
+        birdAccY = 1
+        birdFlapAccY = -14
+        birdHeight = 24
+
+        pipeLeftX = self.__state[2]
+        pipeRightX = pipeLeftX + 52
+        upperPipeY = self.__state[3]
+        lowerPipeY = upperPipeY + 100
+        
+        srceenWidth = 288
+        screenHeight = 512
+        isFlap = self.__state[4]
+        die = False
+        
+        def check_die():
+            # Out screen
+            if birdX < 0 or birdX > srceenWidth or birdY < 0 or birdY >screenHeight:
+                return True
+            # hit upper pipe
+            elif pipeLeftX <= birdX <= pipeRightX and 0 <= birdY <=upperPipeY:
+                return True
+            # hit lower pipe
+            elif pipeLeftX <= birdX <= pipeRightX and lowerPipeY <= birdY <=screenHeight:
+                return True
+            else:
+                return False
+        def print_loc():
+            print("X:%d, Y:%d"%(birdX,birdY))
+        ### Simulate ###
+        # Bird Y accelerate
+        if isFlap:
+            birdVelY += birdFlapAccY + (-1) #-14
+        while not die:
+            birdVelY += birdAccY
+            # check speed in range
+            if birdVelY < -8:
+                birdVelY = -8
+            elif birdVelY > 10:
+                birdVelY = 10
+            # Bird Y move
+            birdY += min(birdVelY, 404 - birdY - birdHeight)
+            # Bird X move
+            birdX += birdVelX
+            # DB
+            print_loc()
+            die = check_die()
+        print("END")
+        print_loc()
+        print(self.__state)
