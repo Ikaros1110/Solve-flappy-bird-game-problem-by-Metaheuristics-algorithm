86552b698f0adb4b2111f4380a05e0f099f33606
Snowball <kai890409@gmail.com>
2021-06-15 12:12:36 +0800

    Update mutation.py

--- mutation.py
+++ mutation.py
@@ -12,28 +12,30 @@ class mutation:
 
     def __call__(self, data, upperBound):
         #data: data sets, upperBound: the data's upper value(lower default for 0)
-        self.__newData = []
+        __newData = []
         for val, upper in zip(data, upperBound):
-<<<<<<< HEAD
             if type(val)==bool: 
-                self.__newData.append(val)
+                __newData.append(val)
                 continue
-=======
-            if type(val)==bool: continue
->>>>>>> ade5aba8baec7d82bd6b608a85a8852cce45a594
             else:
                 nd = self.__sigmoid(random.random()*2-1)#Do Normal distribution
                 #origin in half place(0.5)
                 diff = nd-0.5
-                self.__newData.append(val+diff*(upper-val))
-        return self.__newData
+                __newData.append(val+diff*(upper-val))
+        __newData[0] = round(__newData[0])
+        __newData[1] = round(__newData[1])
+        __newData[2] = round(__newData[2])
+        return __newData
 
-    def test(self, data=[1,2,3,4,5], upperBound=[6,7,8,9,10]):
+    def test(self, data=[1,2,3,4,True], upperBound=[6,7,8,9,True]):
         try:
             print("Try \"__call__\" function is working...")
-            self(data, upperBound)
+            print(self(data, upperBound))
         except:
             print("Error: \"__call__\" function can't work")
 
         print("Test done")
+
+mut = mutation()
+mut.test()
 
